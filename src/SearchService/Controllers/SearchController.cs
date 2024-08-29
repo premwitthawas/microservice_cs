c3bf627dbea6ddfe81ecad63df3e7d7ef0021408
withawas_home <witthawas_dev_home@homework.com>
2024-08-29 21:06:19 +0700

    add pagination and SearchService

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Controllers
+{
+    [Route("api/search")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] string searchTerm,
+        [FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 4)
+        {
+            var query = DB.PagedSearch<Item>();
+            query.Sort(x => x.Ascending(a => a.Make));
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                query.Match(Search.Full, searchTerm).SortByTextScore();
+            }
+            query.PageNumber(pageNumber);
+            query.PageSize(pageSize);
+            var result = await query.ExecuteAsync();
+            return Ok(new
+            {
+                results = result.Results,
+                pageCount = result.PageCount,
+                totalCount = result.TotalCount
+            });
+        }
+    }
+}
